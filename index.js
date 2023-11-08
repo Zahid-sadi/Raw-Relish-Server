6735d04b2f51b0dda5d3be4256646b6e82b18458
Zahid-saadi <zahidbinkashem@gmail.com>
2023-11-09 00:06:12 +0600

    delete method created on cart

--- index.js
+++ index.js
@@ -1,90 +1,87 @@
-const  express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
-const port = process.env.PORT || 3000
-
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
 
 // middlewere
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2hres75.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const itemsCollection = client.db("rawRelishDb").collection("items");
-    const reviewsCollection = client.db("rawRelishDb").collection("reviews");
-    const cartCollection = client.db("rawRelishDb").collection("cart");
+        const itemsCollection = client.db("rawRelishDb").collection("items");
+        const reviewsCollection = client.db("rawRelishDb").collection("reviews");
+        const cartCollection = client.db("rawRelishDb").collection("cart");
 
-    app.get( '/items' , async (req,res)=>{
-        const result = await itemsCollection.find().toArray();
-        res.send(result);
-    })
+        app.get("/items", async (req, res) => {
+            const result = await itemsCollection.find().toArray();
+            res.send(result);
+        });
 
-    app.get( '/review' , async (req,res)=>{
-        const result = await reviewsCollection .find().toArray();
-        res.send(result);
-    })
+        app.get("/review", async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            
+            res.send(result);
+        });
 
+        app.get("/cart", async (req, res) => {
+            const email = req.query.email;
+            console.log(email);
+            if (!email) {
+                res.send([]);
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+           
+        });
 
-    app.get('/cart', async(req, res)=>{
-      const email = req.query.email;
-      if(!email){
-        res.send([])
-      }
-      const query = { email: email }
-      const result = await cartCollection.find((query).toArray())
-      res.send(result)
-    })
+      
 
+        app.post("/cart", async (req, res) => {
+            const item = req.body;
+            const result = await cartCollection.insertOne(item);
+            res.send(result);
+        });
 
-    app.post('/cart', async(req,res)=>{
-      const item = req.body;
-      const result = await cartCollection.insertOne(item)
-      res.send(result)
-    })
+        app.delete('/cart/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
+        })
 
-
-
-
-
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+    res.send("raw is ok");
+});
 
-
-app.get( '/', (req,res)=>{
-    res.send('raw is ok')
-
-})
-
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`raw is ok on ${port}`);
-})
+});
